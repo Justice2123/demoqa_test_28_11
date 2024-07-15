809b3263f933d64732202ca9f32087450f299c12
Alyona Cherkasova <sentyao@yandex.ru>
2024-07-15 22:30:31 +0300

    update3

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -21,7 +21,7 @@ public class TestBase {
         Configuration.browserSize = System.getProperty("resolution", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.pageLoadStrategy = "eager";
-       // Configuration.browserVersion = System.getProperty("browserVersion", "120")
+        Configuration.browserVersion = System.getProperty("browserVersion", "100");
         Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
