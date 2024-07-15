0d267512d10c42aed36b2fd8849270f3c1e47b90
Alyona Cherkasova <sentyao@yandex.ru>
2024-07-15 21:47:59 +0300

    update

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,6 +7,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import helpers.Attachments;
 
 
 import java.util.Map;
@@ -15,9 +16,9 @@ public class TestBase {
 
     @BeforeAll
     static void setUpBefore() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-       // Configuration.timeout = 10000;
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -26,11 +27,15 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
     }
 
-//    @AfterEach
-//    void setUpAfter() {
-//        Selenide.closeWebDriver();
-//    }
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+        Selenide.closeWebDriver();
+    }
 }
